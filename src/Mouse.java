94758a74901cbb68c8c612239e9677ca479df7f4
Carlos1507 <ayala.carlos@pucp.edu.pe>
2024-08-28 12:24:39 -0500

    Mouse añadido

--- src/Mouse.java
+++ src/Mouse.java
@@ -1,2 +1,14 @@
-public class Mouse {
+public class Mouse{
+    private int dpi;
+
+    public int getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(int dpi) {
+        this.dpi = dpi;
+    }
+    public String moverMouser(){
+        return "Se movió";
+    }
 }
